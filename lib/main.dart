086543a9d82265afbb0dda04eac5e5c9c1891e8c
lightScout <Music1:)>
2020-10-17 22:39:24 +0100

    V 1.0.4 Hive initialized - adapter created
    Signed-off-by: lightScout <Music1:)>

--- lib/main.dart
+++ lib/main.dart
@@ -3,11 +3,27 @@ import 'package:cor/model/user_preference_model.dart';
 import 'package:cor/pages/color_pick_page.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:hive/hive.dart';
+import 'package:path_provider/path_provider.dart' as path_provider;
 import 'package:provider/provider.dart';
 
 import 'cubit/cor_cubit.dart';
+import 'model/user_preference.dart';
 
-void main() {
+void main() async {
+  // Hive initialisation
+  WidgetsFlutterBinding.ensureInitialized();
+  final appDocumentDirectory =
+      await path_provider.getApplicationDocumentsDirectory();
+  Hive.init(appDocumentDirectory.path);
+  Hive.registerAdapter(UserPreferenceAdapter());
+  final userPreferenceBox = await Hive.openBox(
+    'userPreference',
+    compactionStrategy: (int total, int deleted) {
+      return deleted > 0;
+    },
+  );
+
   runApp(ChangeNotifierProvider(
       create: (context) => UserPreferenceModel(), child: MyApp()));
 }
